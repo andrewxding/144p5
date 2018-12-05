33be91bf303dd0e5e1e86bb7a5a41bdf26d16d4e
Ben Yang <yang.benjaminh@gmail.com>
2018-12-04 23:43:55 -0800

    locust results

--- mixed_tomcat.py
+++ mixed_tomcat.py
@@ -10,7 +10,7 @@ def getPost(locust):
 def updatePost(locust):
     postid = random.randint(1, 500) 
     prefix = '/editor/post?action=save'
-    url = prefix + 'username=cs144&postid={num}&title=Loading%20Test&body=***Hello%20World!***'.format(num=postid)
+    url = prefix + '&username=cs144&postid={num}&title=Loading%20Test&body=***Hello%20World!***'.format(num=postid)
     locust.client.post(url, name=prefix)
 
 class MyTaskSet(TaskSet):
--- write_tomcat.py
+++ write_tomcat.py
@@ -4,7 +4,7 @@ from locust import HttpLocust, TaskSet
 def updatePost(locust):
     postid = random.randint(1, 500) 
     prefix = '/editor/post?action=save'
-    url = prefix + 'username=cs144&postid={num}&title=Loading%20Test&body=***Hello%20World!***'.format(num=postid)
+    url = prefix + '&username=cs144&postid={num}&title=Loading%20Test&body=***Hello%20World!***'.format(num=postid)
     locust.client.post(url, name=prefix)
 
 class MyTaskSet(TaskSet):
